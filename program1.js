b3307abb3ca4d5a156ec07f8fd1af902d5d67c92
FaizKhancj7 <faizkhan007cj7@gmail.com>
2024-09-25 16:40:05 +0530

    done

--- program1.js
+++ program1.js
@@ -1,11 +1,36 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isValid(s) {
+  // Stack to keep track of opening brackets
+  let stack = [];
+  
+  // Object to map closing brackets to their corresponding opening brackets
+  let bracketMap = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+  };
+  
+  // Traverse through each character in the string
+  for (let char of s) {
+      // If the character is a closing bracket
+      if (bracketMap[char]) {
+          // Pop the top element from the stack if it's not empty, else use a dummy value '#'
+          let topElement = stack.length ? stack.pop() : '#';
+          
+          // If the popped element doesn't match the corresponding opening bracket
+          if (bracketMap[char] !== topElement) {
+              return false;
+          }
+      } else {
+          // If it's an opening bracket, push onto the stack
+          stack.push(char);
+      }
+  }
+  
+  // If the stack is empty, all brackets were matched correctly
+  return stack.length === 0;
+}
 
-module.exports = { isValid };
-
-
+// Test cases
+console.log(isValid("()"));       // Output: true
+console.log(isValid("()[]{}"));   // Output: true
+console.log(isValid("(]"));       // Output: false
--- program2.js
+++ program2.js
@@ -1,10 +1,38 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+  // Map to hold Roman numeral symbols and their corresponding values
+  const romanMap = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  };
+  
+  let total = 0;
+  let prevValue = 0;
 
+  // Iterate through the string from left to right
+  for (let i = 0; i < s.length; i++) {
+      // Get the value of the current Roman numeral
+      let currentValue = romanMap[s[i]];
 
-module.exports={romanToInt}
+      // If the previous numeral is less than the current one, we subtract twice the previous value
+      if (prevValue < currentValue) {
+          total += currentValue - 2 * prevValue;
+      } else {
+          total += currentValue;
+      }
+      
+      // Update the previous value to the current one for the next iteration
+      prevValue = currentValue;
+  }
+
+  return total;
+}
+
+// Test cases
+console.log(romanToInt("III"));       // Output: 3
+console.log(romanToInt("LVIII"));     // Output: 58
+console.log(romanToInt("MCMXCIV"));   // Output: 1994
